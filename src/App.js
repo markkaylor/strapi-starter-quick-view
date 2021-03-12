93a966f0d9359b57db5e12c7de723aaa1b2fe326
Mark Kaylor <mark.kaylor@strapi.io>
2021-03-12 14:58:07 +0100

    Fetch all starters

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import './App.css'
 
 function App() {
+  const [starters, setStarters] = useState([])
+
+
+  function getIssueCountClass(issueCount) {
+    if (issueCount < 5) {
+      return 'success'
+    }
+
+    if (issueCount < 10) {
+      return 'warning'
+    }
+
+    return 'danger'
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get(
+        'https://api.github.com/orgs/strapi/repos',
+        {
+          params: {
+            per_page: 100,
+          },
+        }
+      )
+      const filteredSortedData = data
+        .filter(
+          (repo) => repo.name.startsWith('strapi-starter') && !repo.archived
+        )
+        .map((repo) => repo)
+        .sort((a, b) => (a.open_issues_count < b.open_issues_count ? 1 : -1))
+
+      setStarters(filteredSortedData)
+    }
+    fetchData()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Starter Issues</h1>
+      <div className="starter-grid">
+        {starters &&
+          starters.map((starter) => {
+            const starterName = starter.name.split('-').slice(2).join(' ')
+            return (
+              <div className="starter-card" key={starter.id}>
+                <h3>{starterName} </h3>
+                <div className="starter-issues-container">
+                  <p>Issues + PRs:</p>
+                  <p className={`starter-issue-count ${getIssueCountClass(starter.open_issues_count)}`}>
+                    {starter.open_issues_count}
+                  </p>
+                </div>
+                <div className="link-container">
+                  <a
+                    className="starter-link"
+                    target="_blank"
+                    rel="noreferrer"
+                    href={starter.html_url}
+                  >
+                    Go to repo
+                  </a>
+                  <a
+                    className="starter-link"
+                    target="_blank"
+                    rel="noreferrer"
+                    href={`${starter.html_url}/issues`}
+                  >
+                    Go to issues
+                  </a>
+                </div>
+              </div>
+            )
+          })}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
