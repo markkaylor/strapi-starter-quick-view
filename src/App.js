c0d18afa91a7ab4ca46e5ba619c2a31df8023849
Mark Kaylor <mark.kaylor@strapi.io>
2021-05-03 16:26:39 +0200

    add owners

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react'
-import './App.css'
+import axios from "axios"
+import { useEffect, useState } from "react"
+import ownerData from "./owners.json"
+import "./App.css"
 
 function App() {
   const [repos, setRepos] = useState({ starters: [], templates: [] })
-  const [view, setView] = useState('starters')
+  const [owners] = useState(ownerData.map((owner) => owner))
+  const [view, setView] = useState("starters")
 
   function getIssueCountClass(issueCount) {
     if (issueCount < 5) {
-      return 'success'
+      return "success"
     }
 
     if (issueCount < 10) {
-      return 'warning'
+      return "warning"
     }
 
-    return 'danger'
+    return "danger"
   }
 
   function handleClick(viewUpdate) {
@@ -25,23 +27,24 @@ function App() {
   useEffect(() => {
     const fetchData = async () => {
       const { data } = await axios.get(
-        'https://api.github.com/orgs/strapi/repos',
+        "https://api.github.com/orgs/strapi/repos",
         {
           params: {
             per_page: 100,
           },
         }
       )
+
       const filteredSortedStarters = data
         .filter(
-          (repo) => repo.name.startsWith('strapi-starter') && !repo.archived
+          (repo) => repo.name.startsWith("strapi-starter") && !repo.archived
         )
         .map((repo) => repo)
         .sort((a, b) => (a.open_issues_count < b.open_issues_count ? 1 : -1))
 
       const filteredSortedTemplates = data
         .filter(
-          (repo) => repo.name.startsWith('strapi-template') && !repo.archived
+          (repo) => repo.name.startsWith("strapi-template") && !repo.archived
         )
         .map((repo) => repo)
         .sort((a, b) => (a.open_issues_count < b.open_issues_count ? 1 : -1))
@@ -54,54 +57,66 @@ function App() {
     fetchData()
   }, [])
 
+  function getOwner(url) {
+    return owners.find((owner) => owner.Link === url)
+  }
+
+  console.log(repos)
+
   return (
     <div className="App">
       <h1 className="uppercase">{view} Issues</h1>
-      <div className="issue-button-container">
+      <div className="repo-button-container">
         <button
-          className="issue-button-link"
-          onClick={() => handleClick('starters')}
+          className="repo-button-link"
+          onClick={() => handleClick("starters")}
         >
           Starters
         </button>
         <button
-          className="issue-button-link"
-          onClick={() => handleClick('templates')}
+          className="repo-button-link"
+          onClick={() => handleClick("templates")}
         >
           Templates
         </button>
       </div>
-      <div className="issue-grid">
+      <div className="repo-grid">
         {repos[view] &&
-          repos[view].map((issue) => {
-            const issueName = issue.name.split('-').slice(2).join(' ')
+          repos[view].map((repo) => {
+            const owner = getOwner(repo.html_url)
+
+            const repoName = repo.name.split("-").slice(2).join(" ")
             return (
-              <div className="issue-card" key={issue.id}>
-                <h3>{issueName} </h3>
-                <div className="issues-container">
+              <div className="repo-card" key={repo.id}>
+                <h3>{repoName} </h3>
+                <div className="repos-container">
+                  <p>Owner: </p>
+                  <p>{owner && owner.Owner}</p>
+                </div>
+                <div className="repos-container">
                   <p>Issues + PRs:</p>
                   <p
                     className={`issue-count ${getIssueCountClass(
-                      issue.open_issues_count
+                      repo.open_issues_count
                     )}`}
                   >
-                    {issue.open_issues_count}
+                    {repo.open_issues_count}
                   </p>
                 </div>
                 <div className="link-container">
                   <a
-                    className="issue-link"
+                    className="repo-link"
                     target="_blank"
                     rel="noreferrer"
-                    href={issue.html_url}
+                    href={repo.html_url}
                   >
                     Go to repo
                   </a>
                   <a
-                    className="issue-link"
+                    className="repo-link"
                     target="_blank"
                     rel="noreferrer"
-                    href={`${issue.html_url}/issues`}
+                    href={`${repo.html_url}/issues`}
                   >
                     Go to issues
                   </a>
